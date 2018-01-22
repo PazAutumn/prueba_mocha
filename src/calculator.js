cb2455678f5b71633509bf021a6b17ed16b26527
gabycasti <castillg@gmail.com>
2018-01-22 18:14:02 -0300

    prueba de testeo

---
+++ src/calculator.js
@@ -0,0 +1,19 @@
+objCalculator = {
+  addNumber: function(a, b){
+    return a + b;
+  },
+
+  substractNumber: function(a, b){
+    return a - b;
+  },
+
+  multiplyNumber: function(a, b){
+    return a * b;
+  },
+
+  divideNumber: function(a, b){
+    return a / b;
+  }
+}
+
+module.exports = objCalculator;
